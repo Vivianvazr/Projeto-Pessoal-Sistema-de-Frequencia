2a90cffcee28dcc2bb1a2702a9155d96239dbf15
Vívian Vaz <Vivianvaz5125@gmail.com>
2025-12-02 16:35:24 -0300

    Remove unnecessary semicolon in server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import registryRouter from './routes/registry'
 const app = express()
 const PORT = 3000
 
-app.use(helmet());
+app.use(helmet())
 app.use('/', registryRouter)
 
 app.listen(PORT, () => {
   console.log(`Servidor em http://localhost:${PORT}`)
-});
+})
